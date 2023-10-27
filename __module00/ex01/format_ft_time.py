999a281cee6bdfac392c8d3141f9f71449cdea9c
mychibani <m.yassinechibani@gmail.com>
2023-10-27 19:29:44 +0200

    fix(module00/ex01): cleaner code

--- __module00/ex01/format_ft_time.py
+++ __module00/ex01/format_ft_time.py
@@ -9,13 +9,8 @@ current_time = time.time()
 time_tuple = time.localtime(current_time)
 
 
-month = time.strftime('%b', time_tuple)
-day = time.strftime('%d', time_tuple)
-year = time.strftime('%Y', time_tuple)
-
-
-formated_time = f"{month} {day} {year}"
-
 print("Seconds since January 1, 1970: " + f'{getSecondsSinceEpoch():,.4f}'
       + " or " + f'{getSecondsSinceEpoch():.2e}'
-      + " in scientific notation\n" + formated_time)
+      + " in scientific notation\n" + time.strftime('%b', time_tuple) + " "
+      + time.strftime('%d', time_tuple) + " "
+      + time.strftime('%Y', time_tuple))
